70db75401d28564ec7d99c6c2bfdc3b5e4616055
Thomas Bigot <thomas.bigot@univ-lyon1.fr>
2014-06-13 16:38:29 +0200

    removed debug loop

--- src/ReconcileDuplications.cpp
+++ src/ReconcileDuplications.cpp
@@ -230,18 +230,18 @@ int main(int args, char ** argv)
         //##################################################################################################################
         if (rank >server)
 	  {
-              //  Code useful to use GDB on clients.
-		int z = 0;
-	      //   char hostname[256];
-	      //gethostname(hostname, sizeof(hostname));
-	      std::cout <<"- PID: "<<getpid()<<std::endl;
-	      std::cout <<"-   z: "<<z<<std::endl;
-	      //printf("PID %d on %s ready for attach\n", getpid(), hostname);
-	      // fflush(stdout);
-	      while (0 == z){
-                std::cout << " z=" << z <<std::endl;
-                sleep(5);
-	      }
+//               //  Code useful to use GDB on clients.
+// 		int z = 0;
+// 	      //   char hostname[256];
+// 	      //gethostname(hostname, sizeof(hostname));
+// 	      std::cout <<"- PID: "<<getpid()<<std::endl;
+// 	      std::cout <<"-   z: "<<z<<std::endl;
+// 	      //printf("PID %d on %s ready for attach\n", getpid(), hostname);
+// 	      // fflush(stdout);
+// 	      while (0 == z){
+//                 std::cout << " z=" << z <<std::endl;
+//                 sleep(5);
+// 	      }
       
 	      ApplicationTools::startTimer();
 	      bool debug = ApplicationTools::getBooleanParameter("debug",params,false);
