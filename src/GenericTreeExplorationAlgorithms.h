8edb67ba4dffa0c398dbf42ae92376d929bfec5d
Thomas Bigot <thomas.bigot@univ-lyon1.fr>
2014-09-10 15:40:27 +0200

    Cleaning header: “using namespace” is not allowed in headers.

--- src/GenericTreeExplorationAlgorithms.h
+++ src/GenericTreeExplorationAlgorithms.h
@@ -43,64 +43,58 @@ knowledge of the CeCILL license and that you accept its terms.
 #ifndef _GENERICTREEEXPLORATIONALGORITHM_H_
 #define _GENERICTREEEXPLORATIONALGORITHM_H_
 
-// From PhylLib:
 #include <Bpp/Phyl/Tree.h>
 #include <Bpp/Phyl/App/PhylogeneticsApplicationTools.h>
 #include <Bpp/Phyl/Node.h>
-//#include <Bpp/Phyl/Model/JCnuc.h>
 #include <Bpp/Phyl/Model/Nucleotide/JCnuc.h>
 #include <Bpp/Phyl/Distance/BioNJ.h>
 #include <Bpp/Phyl/Distance/DistanceEstimation.h>
 
-// From NumCalc:
 #include <Bpp/Numeric/VectorTools.h>
 #include <Bpp/Numeric/Random/RandomTools.h>
 #include <Bpp/Numeric/NumConstants.h>
 #include <Bpp/Numeric/Prob/ConstantDistribution.h>
 
-
-//From Seqlib:
 #include <Bpp/Seq/Alphabet/DNA.h>
-
 
 #include "ReconciliationTools.h"
 
 
-using namespace bpp;
-using namespace std;
-
-
-void changeRoot(TreeTemplate<Node> &tree, int newOutGroup);
-std::vector<Node*>  makeSPR(TreeTemplate<Node> &tree, 
+void changeRoot(bpp::TreeTemplate<bpp::Node> &tree, int newOutGroup);
+std::vector<bpp::Node*>  makeSPR(bpp::TreeTemplate<bpp::Node> &tree, 
                             int cutNodeId, int newBrotherId, 
                             bool verbose = true, 
                             bool returnNodesToUpdate = false);
-void makeMuffatoSPR(TreeTemplate<Node> &tree, 
-             Node* cutNode, 
-             Node* newFather, 
+void makeMuffatoSPR(bpp::TreeTemplate<bpp::Node> &tree, 
+             bpp::Node* cutNode, 
+             bpp::Node* newFather, 
              bool verbose = true) ;
 
-void makeNNI(TreeTemplate<Node> &tree, int nodeId);
-void buildVectorOfRegraftingNodes(TreeTemplate<Node> &tree, int nodeForSPR, std::vector <int> & nodeIdsToRegraft);
-std::vector<int> getRemainingNeighbors(const Node * node1, const Node * node2);
-void getRemainingNeighborsUntilDistance(const Node * node1, const Node * node2, int distance, int d, std::vector <int> & neighbors);
-void getNeighboringNodesIdLimitedDistance (TreeTemplate<Node> &tree, int nodeId, int distance, std::vector <int> & neighboringNodeIds);
-//void getRemainingNeighborsUntilDistanceLowerNodes(TreeTemplate<Node> &tree, const Node * node1, const Node * node2, int distance, int d, std::vector <int> & neighbors);
-void getNeighboringNodesIdLimitedDistanceLowerNodes (TreeTemplate<Node> &tree, int nodeId, int distance, std::vector <int> & neighboringNodeIds);
-void buildVectorOfRegraftingNodesLimitedDistance(TreeTemplate<Node> &tree, int nodeForSPR, int distance, std::vector <int> & nodeIdsToRegraft);
-void buildVectorOfRegraftingNodesLimitedDistanceLowerNodes(TreeTemplate<Node> &tree, int nodeForSPR, int distance, std::vector <int> & nodeIdsToRegraft);
-void makeDeterministicModifications(TreeTemplate<Node> &tree, size_t & nodeForNNI, size_t & nodeForSPR, size_t & nodeForRooting);
-void makeDeterministicNNIsAndRootChangesOnly(TreeTemplate<Node> &tree, size_t & nodeForNNI, size_t & nodeForRooting, const bool fixedOutgroupSpecies_) ;
-bool checkChangeHasNotBeenDone(TreeTemplate<Node> &tree, TreeTemplate<Node> *bestTree, size_t & nodeForNNI, 
+void makeNNI(bpp::TreeTemplate<bpp::Node> &tree, int nodeId);
+void buildVectorOfRegraftingNodes(bpp::TreeTemplate<bpp::Node> &tree, int nodeForSPR, std::vector <int> & nodeIdsToRegraft);
+std::vector<int> getRemainingNeighbors(const bpp::Node* * node1, const bpp::Node* * node2);
+void getRemainingNeighborsUntilDistance(const bpp::Node* * node1, const bpp::Node* * node2, int distance, int d, std::vector <int> & neighbors);
+void getNeighboringNodesIdLimitedDistance (bpp::TreeTemplate<bpp::Node> &tree, int nodeId, int distance, std::vector <int> & neighboringNodeIds);
+void getNeighboringNodesIdLimitedDistanceLowerNodes (bpp::TreeTemplate<bpp::Node> &tree, int nodeId, int distance, std::vector <int> & neighboringNodeIds);
+void buildVectorOfRegraftingNodesLimitedDistance(bpp::TreeTemplate<bpp::Node> &tree, int nodeForSPR, int distance, std::vector <int> & nodeIdsToRegraft);
+void buildVectorOfRegraftingNodesLimitedDistanceLowerNodes(bpp::TreeTemplate<bpp::Node> &tree, int nodeForSPR, int distance, std::vector <int> & nodeIdsToRegraft);
+void makeDeterministicModifications(bpp::TreeTemplate<bpp::Node> &tree, size_t & nodeForNNI, size_t & nodeForSPR, size_t & nodeForRooting);
+void makeDeterministicNNIsAndRootChangesOnly(bpp::TreeTemplate<bpp::Node> &tree, size_t & nodeForNNI, size_t & nodeForRooting, const bool fixedOutgroupSpecies_) ;
+bool checkChangeHasNotBeenDone(bpp::TreeTemplate<bpp::Node> &tree, bpp::TreeTemplate<bpp::Node> *bestTree, size_t & nodeForNNI, 
                                size_t & nodeForRooting, std::vector < double >  &NNILks, 
                                std::vector < double >  &rootLks);
-double checkChangeHasNotBeenDone(TreeTemplate<Node> &tree, map<string, double> treesToLogLk);
-void dropLeaves(TreeTemplate<Node> & tree, const std::vector<string> &spToDrop);
-Tree* MRP(const vector<Tree*>& vecTr);
-void rootTreeWithOutgroup (TreeTemplate<Node> &tree, 
-						   const std::vector<std::string> outgroupTaxa) throw ( TreeException );
-bool isTreeRootedWithOutgroup (const TreeTemplate<Node> &tree, const std::vector<std::string> outgroupTaxa) ;
+double checkChangeHasNotBeenDone(bpp::TreeTemplate<bpp::Node> &tree, std::map<std::string, double> treesToLogLk);
+void dropLeaves(bpp::TreeTemplate<bpp::Node> & tree, const std::vector<std::string> &spToDrop);
+bpp::Tree* MRP(const std::vector<bpp::Tree*>& vecTr);
+void rootTreeWithOutgroup (bpp::TreeTemplate<bpp::Node> &tree, 
+						   const std::vector<std::string> outgroupTaxa) throw ( bpp::TreeException );
+bool isTreeRootedWithOutgroup (const bpp::TreeTemplate<bpp::Node> &tree, const std::vector<std::string> outgroupTaxa) ;
 
+/**
+  * Put all the nodes ID of a subtree into a set
+  * @param node the subtree root
+  * @param IDs the set in which to put the list
+  */
 void getNodesInSubtreeIDs(bpp::Node* node,std::set<unsigned int>& IDs);
 
 #endif 
